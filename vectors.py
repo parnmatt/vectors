f920b6d298abff60dceadc6f578a7af560f556cb
Matthew Parnell <matt@parnmatt.co.uk>
2015-11-21 12:58:02 +0000

    Map operators over components

--- vectors.py
+++ vectors.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 
 from collections import namedtuple
+import operator as op
 
 class Vector(namedtuple('Vector', ('x', 'y', 'z'))):
     """A 3-dimensional mathematical vector."""
 
+    @classmethod
+    def _map(cls, func, *iterables):
+        """Map function over the components."""
+        return cls._make(map(func, *iterables))
+
     def __add__(self, v):
         """Add the components of the vectors."""
-        return Vector(self.x + v.x,
-                      self.y + v.y,
-                      self.z + v.z)
+        return self._map(op.add, self, v)
 
     def __sub__(self, v):
         """Subtract the components of the vectors."""
-        return Vector(self.x - v.x,
-                      self.y - v.y,
-                      self.z - v.z)
+        return self._map(op.sub, self, v)
