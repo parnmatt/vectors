81e474c6fc33baac612a6eebc0c8543f8b1e8942
Matthew Parnell <matt@parnmatt.co.uk>
2015-11-21 12:12:22 +0000

    Construct basic Cartesian 3D vector

--- vectors.py
+++ vectors.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 class Vector():
-    pass
+    """A 3-dimensional mathematical vector."""
+
+    def __init__(self, x, y, z):
+        """Initialise with Cartesian coordinates."""
+        self.x = x
+        self.y = y
+        self.z = z
--- vectorstest.py
+++ vectorstest.py
@@ -4,7 +4,18 @@ import unittest
 import vectors
 
 class TestVectors(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        super().setUp()
+        self.v = vectors.Vector(1, 2, 3)
+
+    def tearDown(self):
+        super().tearDown()
+
+    def test_init(self):
+        self.assertEqual(self.v.x, 1)
+        self.assertEqual(self.v.y, 2)
+        self.assertEqual(self.v.z, 3)
 
 
 if __name__ == '__main__':
